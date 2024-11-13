6394a3ec12bc45bf4f5905e857a2b113fcb0c9ee
Marv492005 <75480878+ANGECRACKT@users.noreply.github.com>
2024-11-13 14:45:40 +0100

    Update Marv's Menu.bat

--- Marv's Menu.bat
+++ Marv's Menu.bat
@@ -6,6 +6,7 @@ color C
 echo [INFO] I am not responsible if your PC is damaged.
 echo [INFO] It will come new Updates.
 echo [INFO] The Password is Marv492005.
+echo [INFO] The Other Password is OtherUser
 echo.
 echo.
 echo 1) Continued
@@ -14,9 +15,39 @@ echo.
 echo.
 echo                                                                                                     [Made By Marv492005]
 set /p Warning=choose: 
-if %Warning% == 1 goto Password
-if %Warning% == 2 goto exit
+if %Warning% == 1 goto Userchoose
+if %Warning% == 2 goto Goodbye
 goto Password
+
+:Userchoose
+cls
+title Choose a User
+color F
+echo 1) Marv492005
+echo 2) Other User [IN WORK]
+echo.
+echo 3) Back
+set /p Userchoose=choose: 
+if %Userchoose% == 1 goto Password
+if %Userchoose% == 2 goto Password2
+if %Userchoose% == 3 goto Goodbye
+goto Userchoose
+
+:Password2
+cls
+title Enter The password here
+echo set speechobject=createobject("sapi.spvoice") >%userprofile%\AppData\Local\Temp\welcome.vbs
+echo dim speechobject >>%userprofile%\AppData\Local\Temp\welcome.vbs
+echo speechobject.speak "Please enter the Password %username%" >>%userprofile%\AppData\Local\Temp\welcome.vbs
+timeout 1 >NUL
+start %userprofile%\AppData\Local\Temp\welcome.vbs
+timeout 1 >NUL
+del %userprofile%\AppData\Local\Temp\welcome.vbs
+set /p Password2=Passwort: 
+if %Password2% == OtherUser goto Intro
+echo wrong
+pause
+goto Password2
 
 :Password
 cls
@@ -740,7 +771,8 @@ echo 40) AnyDesk
 echo 41) photopea
 echo 42) Trash Gang
 echo 43) Trash Gang Discord
-echo 44) Close
+echo 44) Onion Browser
+echo 45) Close
 echo.
 echo.
 echo                                                                                                     [Made By Marv492005]
@@ -788,7 +820,8 @@ if %Website% == 40 start https://anydesk.com/de?utm_term=anydesk&utm_campaign=Ge
 if %Website% == 41 start https://www.photopea.com/
 if %Website% == 42 start https://trashga.ng/
 if %Website% == 43 start https://trashga.ng/discord/
-if %Website% == 44 goto MainMenu
+if %Website% == 44 start https://www.torproject.org/de/download/
+if %Website% == 45 goto MainMenu
 goto Website
 
 :Credits
@@ -1000,3 +1033,4 @@ timeout 1 >NUL
 del %userprofile%\AppData\Local\Temp\welcome.vbs
 msg * Man sieht sich.
 goto exit
+
